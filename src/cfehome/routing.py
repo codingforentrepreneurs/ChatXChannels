c15aeb6584fce605bbfca31c7c4ad6c5e5d7d36b
CFE <hello@teamcfe.com>
2018-05-12 17:13:55 -0700

    4 - Websocket Routing

--- src/cfehome/routing.py
+++ src/cfehome/routing.py
@@ -3,7 +3,6 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator, OriginValidator
 
-
 application = ProtocolTypeRouter({ 
     # Websocket chat handler
     'websocket': AllowedHostsOriginValidator(

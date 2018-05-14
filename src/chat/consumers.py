324aad9885d3bbc3fb54640fdf7b537c0a4882d2
CFE <hello@teamcfe.com>
2018-05-14 12:05:55 -0700

    8 - Async Consumer

--- src/chat/consumers.py
+++ src/chat/consumers.py
@@ -1,26 +1,36 @@
 import json
-from channels.consumer import SyncConsumer
+from django.contrib.auth import get_user_model
+from channels.consumer import SyncConsumer, AsyncConsumer
+from channels.db import database_sync_to_async
 
+User = get_user_model()
 
-class ChatConsumer(SyncConsumer):
-    def websocket_connect(self, event):
+class ChatConsumer(AsyncConsumer):
+    async def websocket_connect(self, event):
         # when the socket connects
         print(event)
-        self.send({
+        self.rando_user = await self.get_name()
+        await self.send({
             "type": "websocket.accept"
         })
 
 
-    def websocket_receive(self, event): # websocket.receive
+    async def websocket_receive(self, event): # websocket.receive
         # when the socket connects
         print(event['text'])
+        print(self.rando_user)
         new_message_data = json.loads(event['text'])
         print(new_message_data)
-        self.send({
+        await self.send({
             "type": "websocket.send",
             "text": json.dumps(new_message_data)
         })
 
-    def websocket_disconnect(self, event):
+    async def websocket_disconnect(self, event):
         # when the socket connects
         print(event)
+
+    @database_sync_to_async
+    def get_name(self):
+        return User.objects.all()[0].username
+
